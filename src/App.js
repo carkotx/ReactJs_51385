098ff26dfed2462178c796d11c14ff49f26068c5
carkotx <carkotx@gmail.com>
2023-05-03 13:42:43 -0300

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [count, setCount] = useState(0)
+  let mensajito = 'Te pasaste!'
+  
+  return ( 
+    
+    < div className = "App" >
+
+    <h1 > {count < 10 ? count : mensajito} </h1>
+
+
+    <button onClick = {() => setCount(count + 1)} > + </button> 
     </div>
   );
-}
+  }
+
 
 export default App;
